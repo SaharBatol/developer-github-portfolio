f032fa62c3337902bb314288e7811227a09ea574
SaharBatol <sahar.batol@yahoo.co.uk>
2026-03-22 17:14:32 +0000

    add repo search functionality

--- src/components/RepoCard.jsx
+++ src/components/RepoCard.jsx
@@ -1,20 +1,20 @@
 import { FaStar } from "react-icons/fa6";
 
-const RepoCard = () => {
+const RepoCard = ({ repo }) => {
   return (
     <div className="repo-card">
-      <h3 className="card-heading">Repo Name</h3>
+      <h3 className="card-heading">{repo.name}</h3>
 
       <p className="card-content">
-        {"Description" || "No description provided."}
+        {repo.description || "No description provided."}
       </p>
       <div className="card-bottom-row">
         <p className="card-content">
-          <strong>Language:</strong> {"Repo Language" || "N/A"}
+          <strong>Language:</strong> {repo.language || "N/A"}
         </p>
         <div className="rating-container">
           <FaStar color="gold" size={17} />
-          <p>0</p>
+          <p>{repo.stargazers_count}</p>
         </div>
       </div>
     </div>
--- src/components/RepoList.jsx
+++ src/components/RepoList.jsx
@@ -1,10 +1,48 @@
+import { fetchGithubRepos } from "@/pages/api";
 import RepoCard from "./RepoCard";
+import { useEffect, useState } from "react";
 
 const RepoList = () => {
+  const [repos, setRepos] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    const username = "vercel";
+    fetchGithubRepos(username)
+      .then((res) => res.json())
+      .then((data) => {
+        if (Array.isArray(data)) {
+          setRepos(data);
+        } else {
+          setError(data.message || "Something went wrong");
+        }
+      })
+      .catch((err) => setError(err.message))
+      .finally(() => setLoading(false));
+  }, []);
+
+  const filteredRepos = repos.filter((repo) =>
+    repo.name.toLowerCase().includes(search.toLowerCase()),
+  );
   return (
     <main className="main-repo-container">
       <h1 className="heading">GitHub Repositories</h1>
-      <RepoCard />
+      <input
+        id="search-bar"
+        type="text"
+        placeholder="Search repositories..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      {filteredRepos.length === 0 ? (
+        <p className="subheading">No repositories found.</p>
+      ) : (
+        <div className="repo-card-container ">
+          {filteredRepos.map((repo) => (
+            <RepoCard key={repo.id} repo={repo} />
+          ))}
+        </div>
+      )}
     </main>
   );
 };
